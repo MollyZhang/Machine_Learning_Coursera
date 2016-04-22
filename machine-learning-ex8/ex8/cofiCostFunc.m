72fecab3322dae5bf5ce28762752d6f1ebe0ca03
Molly Zhang <mollyzhang@ucsc.edu>
2016-04-22 13:33:51 -0700

    implment collaborative filtering cost function with regularization

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,7 +41,9 @@ Theta_grad = zeros(size(Theta));
 %
 
 
-J = sum(sum(((X * Theta' - Y).^2) .*R ))/2;
+J = sum(sum(((X * Theta' - Y).^2) .*R ))/2 + ...
+    sum(sum(Theta.^2)) * lambda/2 + ...
+    sum(sum(X.^2)) * lambda/2;
 
 for i = 1:num_movies
     X_grad(i, :) = ((X(i, :) * Theta' - Y(i, :)) .* R(i, :)) * Theta;
